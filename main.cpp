f3656d160e9ffea60eafa54398f9aa0a6f5fe3e2
zouz <2086116928@qq.com>
2019-06-29 22:43:04 +0800

    PUSH QTDEMO

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     cout << "Hello World!" << endl;
+    int i = 0;
+    cout << " i = : " << i << endl;
     return 0;
 }
